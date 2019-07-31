71187186b06a6cbb8d3638fbc6f0ae249fc93151
sanazjamloo <sanaz.jamloo@gmail.com>
2019-07-31 14:12:27 -0700

    try arrow function syntax

--- app.js
+++ app.js
@@ -1,15 +1,14 @@
 // Defining the components as JS function
 //React components are required to start with uppercase letters.
-function Header () {
-  return (
+const Header =  () => 
     <header>
       <h1>Scoreboard</h1>
       <span className="stats">Player: 1</span>
     </header>
-  );
-}
+
+
 
 ReactDOM.render (
-  <Header/>,
+  <Header />,
   document.getElementById("root")
 );
