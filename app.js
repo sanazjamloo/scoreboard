1c6c980f333f828c44dbbbc7c7b38f793c0d3574
sanazjamloo <sanaz.jamloo@gmail.com>
2019-08-01 11:04:59 -0700

    create player component

--- app.js
+++ app.js
@@ -1,11 +1,23 @@
 // Defining the components as JS function
 //React components are required to start with uppercase letters.
-const Header =  () => 
+const Header =  () => {
+  return (
     <header>
       <h1>Scoreboard</h1>
       <span className="stats">Player: 1</span>
     </header>
+  );
+}
 
+const Player () => {
+  return (
+    <div className="player">
+      <span className="player-name">
+        Sanaz
+      </span>
+    </div>
+  );
+}
 
 
 ReactDOM.render (
