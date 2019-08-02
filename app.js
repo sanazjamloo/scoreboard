3eb42f4e6bb5691361c69c3087b0e6a19771330b
sanazjamloo <sanaz.jamloo@gmail.com>
2019-08-02 12:21:47 -0700

    edit class prop syntax

--- app.js
+++ app.js
@@ -46,12 +46,9 @@ const Player = (props) => {
 
 class Counter extends React.Component {
 
-  constructor() {
-    super()
-    this.state = {
-      score: 0
-    };
-  }
+state = {
+  score: 0
+};
 
   render() {
     return (
